9873d824cdcf0cbafc6b2ecfb0799ffaa177e316
Fabio <sstrmn1@gmail.com>
2023-05-15 16:50:38 -0400

    Juego de simon dice completo :)

--- app.js
+++ app.js
@@ -1,52 +1,96 @@
 // Variables
 let juegoComenzado = false;
 let numeroRandom = 0;
-const patron = [];
-const patron2 = [];
+let patron = [];
+let patron2 = [];
+let mensaje = "Presione cualquier boton para empezar";
+let nivel = 0;
 const colores = ["green", "red", "yellow", "blue"];
 
+$("#level-title").text(mensaje);
+
 $(document).on("keydown", function () {
-  if (juegoComenzado === false) {
-    console.log("El juego a empezado");
+  if (juegoComenzado === false) {    
     comenzarJuego();
-    juegoComenzado = true;  
+    juegoComenzado = true;
   }
 });
+
+
 
 colores.forEach((color) => {
   $("#" + color).on("click", function () {
     if (juegoComenzado === true) {
-      patron.push(color);
-
+      patron2.push(color);
       animacionBoton(color);
+      for (let index = 0; index < patron2.length; index++) {
+        if (patron[index] != patron2[index]) {
+          gameOver();
+        } else if (patron.length === patron2.length) {
+          // debugger;
+          setTimeout(function () {}, 2000);
 
-      console.log(patron);
+          let colorAleatorio = colorRandom();
+
+          setTimeout(() => {
+            animacionBoton(colorAleatorio);
+          }, 1000);
+
+          patron.push(colorAleatorio);
+          patron2 = [];
+          nivel++;
+          mensaje = `Nivel ${nivel}`;
+          $("#level-title").text(mensaje);
+          // setTimeout(generarColorRandom, 1000);
+        }
+      }
+    } else {
+      console.log("presione tecla para comenzar juego");
     }
   });
 });
 
 // Funciones
 function comenzarJuego() {
-  let color = "";
+  setTimeout(generarColorRandom, 1000);
+  mensaje = `Nivel ${nivel}`;
+  $("#level-title").text(mensaje);
+}
+
+function generarColorRandom() {
+  let colorRandomico = colorRandom();
+  animacionBoton(colorRandomico);
+  patron.push(colorRandomico);
+}
+
+function sonIguales(arreglo1, arreglo2) {
+  // Verificar si tienen la misma longitud
+  if (arreglo1.length !== arreglo2.length) {
+    return false;
+  }
+
+  // Verificar si todos los elementos son iguales en las mismas posiciones
+  return arreglo1.every((elemento, indice) => elemento === arreglo2[indice]);
+}
+
+function colorRandom() {
   numeroRandom = Math.floor(Math.random() * 4);
   switch (numeroRandom) {
     case 0:
-      color = "green";
-      break;
+      return "green";
+
     case 1:
-      color = "red";
-      break;
+      return "red";
+
     case 2:
-      color = "yellow";
-      break;
+      return "yellow";
+
     case 3:
-      color = "blue";
-      break;
+      return "blue";
 
     default:
       break;
   }
-  animacionBoton(color);
 }
 
 function animacionBoton(color) {
@@ -54,4 +98,15 @@ function animacionBoton(color) {
   setTimeout(function () {
     $("#" + color).removeClass("pressed");
   }, 100);
+}
+
+function gameOver() {
+  console.log("Fracaso");
+  juegoComenzado = false;
+  patron = [];
+  patron2 = [];
+  nivel = 0;
+  mensaje = `¡¡Game Over!! Presione una tecla para volver a empezar :) `;
+
+  $("#level-title").text(mensaje);
 }
