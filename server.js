20537104cde922d0be25ab66eeb4c058e4be4849
Fabio <sstrmn1@gmail.com>
2023-05-16 14:37:39 -0400

    mover los archivos simon styles  a un folder publico, usar folderes estaticos en servidor

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  req.send("Hello world!");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, function () {
